55bb1d3c1b1fe9303c21a8500be5d4d3037e5f0e
João Dias <joaoeduardodias123@gmail.com>
2023-06-21 11:23:18 -0300

    feat: add theme options

---
+++ web/src/data/themeOptions.ts
@@ -0,0 +1,10 @@
+export const THEME_OPTIONS = {
+  LOVE: 'Amor de Deus',
+  FAITH_AND_CONVERSION: 'Fé e conversão',
+  SIN_AND_SALVATION: 'Pecado e salvação',
+  LORDSHIP_OF_JESUS: 'Senhorio de Jesus',
+  JESUS_SAVIOR: 'Jesus Salvador',
+  HOLY_SPIRIT: 'Espírito Santo',
+  COMMUNITY: 'Comunidade',
+  MARIA: 'Maria',
+}
